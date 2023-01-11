c9323cde56e8005852ea26ff26a7a5a313ac79f5
zhufucdev <1095291434@qq.com>
2023-01-11 16:16:57 +0800

    Migrated to global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,30 @@
 declare global {
+    type UserID = string;
+    type TokenID = string;
+    type RecentID = string;
+    type InspirationID = string;
+    type ImageID = string;
+    type PrivateMessageID = string
+
+    type MessageType = 'inspiration' | 'recent' | 'pm' | 'issue';
+    type PermissionID =
+        | "raise_inspiration"
+        | "raise_issue"
+        | "raise_recent"
+        | "send_pm"
+        | "comment"
+        | "remark"
+        | "edit_own_post"
+        | "change_nick"
+        | "change_avatar"
+        | "change_biography"
+        | "list_images"
+        | "permit"
+        | "modify"
+        | "default"
+        | "*";
+
+    type RequestResult = { success: boolean, respond?: string, msg?: string };
 }
 
 export {};
