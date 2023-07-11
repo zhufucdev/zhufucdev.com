33d9cd9a0ebef1f60db7bbfb5a2e1f1d2fefc3cf
zhufucdev <1095291434@qq.com>
2023-07-12 00:13:22 +0800

    Update test

--- __tests__/tagging.ts
+++ __tests__/tagging.ts
@@ -5,7 +5,7 @@ import {readTags, stringifyTags, TagKey, Tags} from "../lib/tagging";
 const magicId = 'qwertyui000';
 test('should read tags', () => {
     const tags = [`t-from: ${magicId}`, `lang: en-US`, 'hidden']
-    const parsed = readTags({tags});
+    const parsed = readTags(tags);
 
     expect(parsed[TagKey.Hidden]).toBeTruthy();
     expect(parsed[TagKey.TranslatedFrom]).toBe(magicId);
