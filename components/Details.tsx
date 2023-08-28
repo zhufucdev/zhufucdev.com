5591cb76728ea4448b83b1e41ebfc8c7f0b022ea
zhufucdev <1095291434@qq.com>
2023-08-28 08:17:11 +0800

    Feat: sx and inline for Details

--- components/Details.tsx
+++ components/Details.tsx
@@ -6,6 +6,7 @@ import {
     Dialog,
     DialogContent,
     DialogTitle,
+    SxProps,
 } from '@mui/material'
 import { ReactNode, useState } from 'react'
 import HelperCard from './HelperCard'
@@ -14,21 +15,31 @@ interface Props {
     variant?: 'dialog' | 'collapse'
     title: ReactNode
     children: ReactNode
+    sx?: SxProps
     buttonProps?: ButtonProps
+    inline?: boolean
 }
 
-export default function Details({ variant, title, children, buttonProps }: Props) {
+export default function Details({
+    variant = 'collapse',
+    title,
+    children,
+    sx,
+    inline,
+    buttonProps,
+}: Props) {
     const [expanded, setExpanded] = useState(false)
     const buttonContent = expanded ? '隐藏详细' : '显示详细'
     switch (variant) {
         case 'dialog':
+            const button = (
+                <Button onClick={() => setExpanded(!expanded)} {...buttonProps}>
+                    {buttonContent}
+                </Button>
+            )
             return (
                 <>
-                    <Box>
-                        <Button onClick={() => setExpanded(!expanded)} {...buttonProps}>
-                            {buttonContent}
-                        </Button>
-                    </Box>
+                    {inline ? button : <Box sx={sx}>{button}</Box>}
                     <Dialog open={expanded} onClose={() => setExpanded(false)}>
                         <DialogTitle>{title}</DialogTitle>
                         <DialogContent>{children}</DialogContent>
@@ -37,12 +48,14 @@ export default function Details({ variant, title, children, buttonProps }: Props
             )
 
         case 'collapse':
-        default:
             return (
-                <HelperCard title={title}>
+                <HelperCard title={title} sx={sx}>
                     <Collapse in={expanded}>{children}</Collapse>
                     <Box width="100%" display="flex" justifyContent="right">
-                        <Button onClick={() => setExpanded(!expanded)} {...buttonProps}>
+                        <Button
+                            onClick={() => setExpanded(!expanded)}
+                            {...buttonProps}
+                        >
                             {buttonContent}
                         </Button>
                     </Box>
