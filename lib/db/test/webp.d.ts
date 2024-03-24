421be2ac306710ea03737f1b691862ca30714f42
zhufucdev <1095291434@qq.com>
2024-03-24 10:01:28 +0800

    :arrow_up: use sharp instead

--- lib/db/test/webp.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'webp-converter' {
-  declare var main: {
-    grant_permissions(),
-    buffer2webpbuffer(input: Buffer, format: string, options: string): Promise<Buffer>
-  }
-  export default main
-}
